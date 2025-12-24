b68a3c177e71103fcfc0c6d804ee8dc9ff909e83
Bruno Luz <bdaluzferreira@gmail.com>
2025-12-23 22:32:29 -0300

    feat: implement redis caching in card service

--- Application/Services/CardService.cs
+++ Application/Services/CardService.cs
@@ -13,16 +13,22 @@ namespace Application.Services
     public class CardService : ICardService
     {
         private readonly ICardRepository _repository;
-        public CardService(ICardRepository cardRepository) { 
-            _repository = cardRepository;        
+        private readonly ICacheService _cacheService;
+
+        public CardService(ICardRepository cardRepository, ICacheService cacheService)
+        {
+            _repository = cardRepository;
+            _cacheService = cacheService;
         }
 
         public async Task<CardDto> CreateCardAsync(CreateCardDto dto)
         {
             var card = new Card(dto.FrontText, dto.BackText, dto.DeckId);
+            var cacheKey = $"cards_deckKey_{dto.DeckId}";
 
             await _repository.AddAsync(card);
             await _repository.SaveChangesAsync();
+            await _cacheService.RemoveAsync(cacheKey);
 
             return new CardDto
             {
@@ -36,9 +42,16 @@ namespace Application.Services
 
         public async Task<List<CardDto>> GetAllCardsByDeckId(Guid deckId)
         {
+            var cacheKey = $"cards_deckKey_{deckId}";
+            var cachedCards = await _cacheService.GetAsync<List<CardDto>>(cacheKey);
+
+            if (cachedCards != null) { 
+                return cachedCards;
+            }
+
             var cards = await _repository.GetByDeckIdAsync(deckId);
 
-            return cards.Select(c => new CardDto
+            var cardDtos = cards.Select(c => new CardDto
             {
                 Id = c.Id,
                 FrontText = c.FrontText,
@@ -46,6 +59,10 @@ namespace Application.Services
                 DeckId = deckId,
                 NextReviewDate = c.NextReviewDate
             }).ToList();
+
+            await _cacheService.SetAsync(cacheKey, cardDtos);
+
+            return cardDtos;
         }
     }
 }
